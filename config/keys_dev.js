62ba6ace30b9f89573555e6474c201aed3742e60
sam <asad.bsse2993@iiu.edu.pk>
2019-10-20 10:43:21 -0700

    settings

---
+++ config/keys_dev.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   mongoURI: 'mongodb://bridal:bridal123@ds143262.mlab.com:43262/bridalnetwork',
+//   secretOrKey: 'secret'
+// };
+
+
+module.exports = {
+  mongoURI: 'mongodb+srv://ahmadjohn89:e6Jufd5uSPGw2wse@cluster0-anxd6.mongodb.net/bridalnetwork?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
